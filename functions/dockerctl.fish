07b8d5bde9ab00058498cecebbeb37ed797bf7ef
tatsuo48 <green.americanidiot.yokoyama@gmail.com>
2021-03-29 20:22:16 +0900

    fix

--- functions/dockerctl.fish
+++ functions/dockerctl.fish
@@ -1,4 +1,4 @@
 function dockerctl
   set -l containerID (docker ps |peco --prompt "接続先コンテナを選択して下さい。 >" | awk '{print $1}')
-  docker exec -it $containerID /bin/bash || echo "bashからshに切り替えて再試行します。" && docker exec -it $containerID /bin/sh
+  docker exec -it $containerID /bin/bash || docker exec -it $containerID /bin/sh
 end
